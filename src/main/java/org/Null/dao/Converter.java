c64de44ea2e9bdaa48968c30c6868118483ccb5d
Arkadij Kuzhel <arkadij.k@ontapgroup.com>
2023-09-08 20:11:17 +0300

    ...

---
+++ src/main/java/org/Null/dao/Converter.java
@@ -0,0 +1,29 @@
+package org.Null.dao;
+import java.io.*;
+import java.util.*;
+
+
+
+
+public class Converter {
+    private File mainFile;
+    private char divider;
+
+    private HashMap<String, String> translations = new HashMap<>();
+
+
+    public Converter(File mainFile, char divider) {
+        this.mainFile = mainFile;
+        this.divider = divider;
+    }
+
+    public HashMap convert() {
+        StringBuffer stringBuffer = new StringBuffer();
+
+        
+
+
+
+        return translations;
+    }
+}
